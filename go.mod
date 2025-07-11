a28a16ec620bf37739231bfd69225c2a6bb6e976
makki_d <d.makiuchi@gmail.com>
2025-07-11 17:32:45 +0900

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/makiuchi-d/tinygo-concentration
+
+go 1.24.5
+
+require github.com/tinygo-org/pio v0.0.0-20250223134105-56a020e0a934
---
+++ main.go
@@ -0,0 +1,211 @@
+package main
+
+import (
+	"machine"
+	"math/rand"
+	"time"
+
+	pio "github.com/tinygo-org/pio/rp2-pio"
+	"github.com/tinygo-org/pio/rp2-pio/piolib"
+)
+
+type WS2812B struct {
+	*piolib.WS2812B
+	Pin machine.Pin
+}
+
+func NewWS2812B(pin machine.Pin) *WS2812B {
+	s, _ := pio.PIO0.ClaimStateMachine()
+	ws, _ := piolib.NewWS2812B(s, pin)
+	ws.EnableDMA(true)
+	return &WS2812B{WS2812B: ws}
+}
+
+type card struct {
+	color   uint32
+	removed bool
+	open    bool
+}
+
+type cards []card
+
+func newCards(hard bool) cards {
+	n1, _ := machine.GetRNG()
+	n2, _ := machine.GetRNG()
+	rng := rand.New(rand.NewSource(int64(n1)<<32 + int64(n2)))
+
+	var cs cards
+	if hard {
+		cs = make([]card, 12)
+		for i := range 6 {
+			clr := randColor(rng)
+			cs[i*2].color = clr
+			cs[i*2+1].color = clr
+		}
+	} else {
+		cs = cards{
+			card{color: 0xff0000ff}, card{color: 0xff0000ff},
+			card{color: 0x00ff00ff}, card{color: 0x00ff00ff},
+			card{color: 0x0000ffff}, card{color: 0x0000ffff},
+			card{color: 0xffff00ff}, card{color: 0xffff00ff},
+			card{color: 0xff00ffff}, card{color: 0xff00ffff},
+			card{color: 0x00ffffff}, card{color: 0x00ffffff},
+		}
+	}
+
+	rng.Shuffle(len(cs), func(i, j int) {
+		cs[i], cs[j] = cs[j], cs[i]
+	})
+	return cs
+}
+
+func randColor(rng *rand.Rand) uint32 {
+	for {
+		c := rng.Uint32() & 0xf0f0f000
+		c = c | (c >> 8) | 0x000000ff
+		if c != 0x000000ff && c != 0xffffffff {
+			return c
+		}
+	}
+}
+
+func (cs cards) getRaw() []uint32 {
+	r := make([]uint32, 12)
+	for i := range 12 {
+		if cs[i].removed {
+			r[i] = 0x00000000
+			continue
+		}
+		if !cs[i].open {
+			r[i] = 0xffffffff
+			continue
+		}
+		r[i] = cs[i].color
+	}
+	return r
+}
+
+var (
+	colPins = []machine.Pin{
+		machine.GPIO5,
+		machine.GPIO6,
+		machine.GPIO7,
+		machine.GPIO8,
+	}
+	rowPins = []machine.Pin{
+		machine.GPIO9,
+		machine.GPIO10,
+		machine.GPIO11,
+	}
+)
+
+func initPins() {
+	for _, p := range colPins {
+		p.Configure(machine.PinConfig{Mode: machine.PinOutput})
+		p.Low()
+	}
+	for _, p := range rowPins {
+		p.Configure(machine.PinConfig{Mode: machine.PinInputPulldown})
+	}
+}
+
+func waitKey() int {
+	for {
+		for i := range 4 {
+			for c, p := range colPins {
+				if c == i {
+					p.High()
+				} else {
+					p.Low()
+				}
+			}
+			time.Sleep(1 * time.Millisecond)
+			for r, p := range rowPins {
+				if p.Get() {
+					return i*3 + r
+				}
+			}
+		}
+		time.Sleep(10 * time.Millisecond)
+	}
+}
+
+func waitCloseKey(cs cards) int {
+	for {
+		p := waitKey()
+		if cs[p].removed || cs[p].open {
+			continue
+		}
+		return p
+	}
+}
+
+func main() {
+	initPins()
+	ws := NewWS2812B(machine.GPIO1)
+	ws.WriteRaw([]uint32{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0})
+	time.Sleep(time.Second / 2)
+	cs := newCards(true)
+
+	num := 6
+
+	for {
+		ws.WriteRaw(cs.getRaw())
+
+		// first card
+		p1 := waitCloseKey(cs)
+		cs[p1].open = true
+		ws.WriteRaw(cs.getRaw())
+
+		// second card
+		p2 := waitCloseKey(cs)
+		cs[p2].open = true
+		ws.WriteRaw(cs.getRaw())
+
+		if cs[p1].color == cs[p2].color {
+			time.Sleep(time.Second / 2)
+			cs[p1].removed = true
+			cs[p2].removed = true
+			ws.WriteRaw(cs.getRaw())
+			time.Sleep(time.Second / 2)
+			cs[p1].removed = false
+			cs[p2].removed = false
+			ws.WriteRaw(cs.getRaw())
+			time.Sleep(time.Second / 2)
+			cs[p1].removed = true
+			cs[p2].removed = true
+			ws.WriteRaw(cs.getRaw())
+			time.Sleep(time.Second / 2)
+			cs[p1].removed = false
+			cs[p2].removed = false
+			ws.WriteRaw(cs.getRaw())
+			time.Sleep(time.Second / 2)
+			cs[p1].removed = true
+			cs[p2].removed = true
+
+			num--
+			if num == 0 {
+				break
+			}
+		} else {
+			time.Sleep(time.Second * 2)
+			cs[p1].open = false
+			cs[p2].open = false
+		}
+	}
+
+	ws.WriteRaw(cs.getRaw())
+	time.Sleep(time.Second / 2)
+
+	for n := range cs {
+		cs[n].removed = false
+		cs[n].open = true
+	}
+	ws.WriteRaw(cs.getRaw())
+
+	time.Sleep(time.Second * 5)
+	for n := range cs {
+		cs[n].removed = true
+	}
+	ws.WriteRaw(cs.getRaw())
+}
